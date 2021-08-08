05f9e239c804ef0e823ea6eeeabb161fa557330b
My Name <thesame@email.com>
2021-08-08 17:10:57 +1200

    master

--- WebofScience/Python/Sentimental_Analysis.py
+++ WebofScience/Python/Sentimental_Analysis.py
@@ -1,35 +1,64 @@
 from textblob import TextBlob
 import PyPDF2
+import docx2txt
+import fitz
 
 
-pdfFileObj = open("/home/caleb/Desktop/University/INFO301/Git/WebOfScience/WebofScience/Python/Testing.pdf", 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj, strict=False)
-if pdfReader.isEncrypted:
-    pdfReader.decrypt('')
+#Old PDFFILE to TEXT format
+#pdfFileObj = open("Spacing.pdf", 'rb')
+#pdfReader = PyPDF2.PdfFileReader(pdfFileObj, strict=False)
+#if pdfReader.isEncrypted:
+#    pdfReader.decrypt('')  
+#count = pdfReader.numPages
+#for i in range(count):
+#    page = pdfReader.getPage(i)  
+#pageObj = pdfReader.getPage(i)
+#print (pageObj.extractText())
+#SentimentScore = TextBlob(pageObj.extractText())
+#print (SentimentScore.sentiment)
 
-pageObj = " "
-count = pdfReader.numPages
+#Word to Text
+docxFileObj = docx2txt.process("test.docx")
+
+
+#PDF To Text
+pdf_doc ="Testing.pdf"
+doc = fitz.open(pdf_doc)
+count = doc.pageCount
+#ask for Text_total Location
 for i in range(count):
-    page = pdfReader.getPage(i)
-    pageObj += page.extractText()
-
+    text_total = " "
+    page = doc.loadPage(i)
+    pdf_page = page
+    text = pdf_page.getText('text')
+    text_total = (text_total + " " + text)
+    
+pageObj = text_total
 SentimentScore = TextBlob(pageObj)
 print (SentimentScore.sentiment)
-
 
 
 # The polarity score is a float within the range [-1.0, 1.0].
 if SentimentScore.sentiment.polarity > 0:
     print ("Positive")
+    SS_P = ("Positive")
 elif SentimentScore.sentiment.polarity < 0:
     print ("Negative")
+    SS_P = ("Negative")
 else:
     print ("Neutral")
-    
+    SS_P = ("Neutral")
 #  The subjectivity is a float within the range [0.0, 1.0] where 0.0 is very objective and 1.0 is very subjective.
 if SentimentScore.sentiment.subjectivity >= 0.5:
-    print ("Objective")
-elif SentimentScore.sentiment.subjectivity < 0.5:
     print ("subjective")
+    SS_S = ("subjective")
+elif SentimentScore.sentiment.subjectivity < 0.5:
+    print ("Objective")
+    SS_S =  ("Objective")
 else:
     print ("Woops this shouldn't be happening")
+
+    
+
+
+
