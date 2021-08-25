5aabc0538ba072a2b8db46778566d4c8648c8eb2
boydb <boydb@192.168.20.11>
2021-08-25 22:52:19 +1200

    Fixing mistake made so redoing DAO's #2

--- WebofScience/src/main/java/dao/userDAO.java
+++ WebofScience/src/main/java/dao/userDAO.java
@@ -5,10 +5,17 @@
  */
 package dao;
 
+import domain.Article;
+import domain.User;
+
 /**
  *
  * @author boydb
  */
-public class userDAO {
-    
+public interface userDAO {
+    public void addArticle(Article article);
+    public void addContributer(User user);
+    public void addAccount(User user);
+    public void flagArticle(Article article);
+    public void deleteAccount(User user);
 }
