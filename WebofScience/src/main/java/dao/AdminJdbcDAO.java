5bdb4a18202d8e190dcdec27118389a77cfab83f
Ryan Lau <Amy@jareds-mbp.hub>
2021-09-07 15:45:36 +1200

    Method add verifier

--- WebofScience/src/main/java/dao/AdminJdbcDAO.java
+++ WebofScience/src/main/java/dao/AdminJdbcDAO.java
@@ -32,6 +32,31 @@ public class AdminJdbcDAO implements AdminDAO {
     }
     
     public void addVerifier(Verifier verifier) {
+        String sql = "insert into User (username, fname, lname, email, password, dob, gender, institute, department, field_of_research, roleId) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, (select role_id from role where name='Verifier'))";
+ 
+        try (
+                Connection dbCon = DbConnection.getConnection(uri);
+                PreparedStatement stmt = dbCon.prepareStatement(sql);) {
+ 
+            stmt.setString(1, verifier.getUsername());
+            stmt.setString(2, verifier.getFirstName());
+            stmt.setString(3, verifier.getLastName());
+            stmt.setString(4, verifier.getEmail());
+            stmt.setString(5, verifier.getPassword());
+            stmt.setString(6, verifier.getDob().toString());
+            stmt.setString(7, verifier.getGender());
+            stmt.setString(8, verifier.getInstitution());
+            stmt.setString(9, verifier.getDeptName());
+            stmt.setString(10, verifier.getFieldResearch());
+            
+            //not needed since nested query will get it for us.
+            //stmt.setInt(11, user.getRole().getRoleID());
+ 
+            stmt.executeUpdate();
+ 
+        } catch (SQLException ex) {
+            throw new DAOException(ex.getMessage(), ex);
+        }
         
     }
     
