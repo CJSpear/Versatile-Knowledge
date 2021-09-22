6a8c4226b8bcdeb79daf2b5156fa50e09c59ee7a
sarahaverill <sarahaverill@sarahavrillsmbp.student.uod.otago.ac.nz>
2021-09-23 10:22:22 +1200

    Article Module

--- WebofScience/src/main/java/web/ArticleModule.java
+++ WebofScience/src/main/java/web/ArticleModule.java
@@ -16,8 +16,38 @@ import org.jooby.Jooby;
  * @author Saverill
  */
 public class ArticleModule extends Jooby{
+  public ArticleModule(ArticleJdbcDAO articleDao) {
     
+    //upload (POST) article 
+     post("/api/articles", (req, rsp) -> {
+            Article article = req.body().to(Article.class);
+            articleDao.addArticle(article);
+            rsp.status(Status.CREATED);
+        });
+            
+   //Delete article 
+    delete("/api/articles/:id", (req, rsp) -> {
+            String id = req.param("id").value();
+            Article article = article.getArticleId(Integer.parseInt(id));
+            articleDao.deleteArticle(article);
+            rsp.status(Status.NO_CONTENT);
+            
+        });
+    
+   //edit article 
+   put("/api/updateArt/:id", (req, rsp) -> {
+            Article article = req.body().to(Article.class);
+            articleDao.updateArticle(article);
+            rsp.status(Status.CREATED);
+        });
+   
+   //flag artcile
+   
+   
+   //add contributor
      
+                 
+       
  }  
 }
 
