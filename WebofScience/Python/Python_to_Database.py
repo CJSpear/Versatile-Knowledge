d7eec32ab4c85f7db3a50277ad89b268e6086725
My Name <thesame@email.com>
2021-09-23 10:49:18 +1200

    Search_Function

--- WebofScience/Python/Python_to_Database.py
+++ WebofScience/Python/Python_to_Database.py
@@ -11,12 +11,20 @@ connection  = jaydebeapi.connect(
         "WebofScience/Python/h2-latest.jar")
 
 cursor = connection.cursor()
-#Search Function
 
+
+
+
+SearchAmount = 0
+#Search Function
+val = input (" ENTER ")
+
+
+#Search all in TABLE
 def res():
         try:
                 result = None
-                cursor.execute("SELECT CUST_ID,NAME FROM WORK")
+                cursor.execute("SELECT CUST_ID,NAME FROM WORK".format(val))
                 result = cursor.fetchall()
                 return result
         except Error as err:
@@ -26,10 +34,33 @@ results = res()
 from_db = []
 
 for result in results:
+        SearchAmount  +=1
         result = result
         from_db.append(result)
 
 print (from_db)
+print (SearchAmount)
+
+#Search For Specific Result
+def SearchRes():
+        try:
+                result = None        
+                cursor.execute("SELECT * FROM WORK WHERE NAME = 'John'")
+                result = cursor.fetchall()
+                return result
+        except Error as err:
+                print (f'item error: "{err} "')
+
+results = SearchRes()
+from_db = []
+
+for result in results:
+        SearchAmount  +=1
+        result = result
+        from_db.append(result)
+
+print (from_db)
+print (SearchAmount)
 
 cursor.close()
 connection.close()
