17c023783bfa7c6f9f40d54d634fbe10919c4e7f
ryanl12 <77248587+ryanl12@users.noreply.github.com>
2021-09-23 03:15:24 +1200

    skeleton of module

--- WebofScience/src/main/java/web/AdminModule.java
+++ WebofScience/src/main/java/web/AdminModule.java
@@ -5,11 +5,21 @@ package web;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import dao.AdminJdbcDAO;
+import domain.Article;
+import java.io.ObjectInputFilter.Status;
+import org.jooby.Jooby;
+
 
 /**
  *
  * @author ryanl
  */
-public class AdminModule {
+public class AdminModule extends Jooby{
     
+    public AdminModule(AdminJdbcDAO articleDao) {
+    
+    
+            
+    }  
 }
