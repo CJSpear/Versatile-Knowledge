a89302f933003f0b4b2c09f8f96dc4e0f792b2b9
My Name <thesame@email.com>
2021-10-11 15:06:03 +1300

    ss

--- WebofScience/public/js/search.js
+++ WebofScience/public/js/search.js
@@ -1,33 +1,25 @@
-
-var Searchitems = angular.module('searching', []);
-
+var Searchitems = angular.module('searching', ['ngResource', 'ngStorage']);
 
 console.log("hello")
 
-/*fetch('/WebofScience/{type}/{val}/Python/callPython.java')
-    .then(res=> console.log(res))
-
-
-
-fetch('/WebofScience/Python/callPython.java', {
-    method: "POST",
-    headers: {  'Content-Type' : 'application/json' },
-    body: JSON.stringify({
-        name: "User 1"
-    })
-}).then(res => {
-    return res.json
-}) .then(data=> console.log(data))
-*/
 
 Searchitems.factory('searchAPI', function($resource){
-    return $resource("WebofScience/src/main/java/web/SearchModule.java");
+
+    return $resource("/api/search/:type/:val");
+
  });
 
 Searchitems.controller("SearchController", ['$scope', '$http', function($scope, $http, searchAPI){
+
+
+
+    $http.get('js/json/messages.json').then(function (data){
+
+        $scope.messages = data.data;
+
+
+        
     
-
-
 
   // $scope.searchFunction({
     //     type: $scope.SearchType,
@@ -36,20 +28,5 @@ Searchitems.controller("SearchController", ['$scope', '$http', function($scope, 
     //    })
     // });
 
-
-
-
-    $http.get('public/json/messages.json').then(function (data){
-
-        $scope.messages = data.data;
-
-    });
-
-}]);
-
-
-function greet(name, element){
-    console.log('hey ${name}!');
-    element.$server.greet("server")
-    element.innerHTML = "it workss";
-}
+})}
+])
