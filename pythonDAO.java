96050bab2992ae524a66c08661c7ea1b8709a0d4
Caleb Spear <speca932@student.otago.ac.nz>
2021-10-14 23:59:34 +1300

    Add files via upload

---
+++ pythonDAO.java
@@ -0,0 +1,21 @@
+// package dao;
+
+// import domain.callPython;
+
+// public class pythonDAO {
+
+//     public Object getType(String type) {
+//         return null;
+//     }
+
+//     public Object getVal(String val) {
+//         return null;
+//     }
+//     public Object getVal(String val);
+//     public Object getType(String type);
+
+
+
+
+
+// }
